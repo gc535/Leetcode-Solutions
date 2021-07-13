0454a47bae45541eb60372f70c108dfbf09c9bc7
Guangwei Chen <31547415+gc535@users.noreply.github.com>
2021-07-13 16:55:04 -0400

    Update attempt1.py

--- 1911.Maximum_Alternating_Subsequence_Sum/attempt1.py
+++ 1911.Maximum_Alternating_Subsequence_Sum/attempt1.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def maxAlternatingSum(self, nums):
-        maxVal = [[0, 0]] # max val you can get for the current array if it has odd len or even len
+        maxVal = [0, 0] # max val you can get for the current array if it has odd len or even len
         for n in nums:
-            maxVal.append([max(maxVal[-1][1]+n, maxVal[-1][0]), max(maxVal[-1][0]-n, maxVal[-1][1])])
-        return max(maxVal[-1])
+            maxVal[0], maxVal[1] = max(maxVal[1]+n, maxVal[0]), max(maxVal[0]-n, maxVal[1])
+        return max(maxVal)
